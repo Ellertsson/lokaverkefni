399c859bc305a2c1225c1cb391e773c81d547d5f
ellertsson <benediktel246@nemi.tskoli.is>
2016-11-24 16:08:28 +0000

    GSÖ_index.php breytt og spurning bætar við

--- GSÖ_quiz/GSÖ_index.php
+++ GSÖ_quiz/GSÖ_index.php
@@ -21,73 +21,73 @@
             
                 <li>
                 
-                    <h3>1.) What does CSS stand for?</h3>
+                    <h3>1.) What does PHP stand for?</h3>
                     
                     <div>
                         <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
-                        <label for="question-1-answers-A">A) Computer Styled Sections </label>
+                        <label for="question-1-answers-A">A) Personal Hypertext Processor </label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
-                        <label for="question-1-answers-B">B) Cascading Style Sheets</label>
+                        <label for="question-1-answers-B">B) PHP: Hypertext Preprocessor</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
-                        <label for="question-1-answers-C">C) Crazy Solid Shapes</label>
+                        <label for="question-1-answers-C">C) Private Home Page</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
-                        <label for="question-1-answers-D">D) Ekkert af þessum</label>
+                        <label for="question-1-answers-D">D) None of the above</label>
                     </div>
                 
                 </li>
                 
                 <li>
                 
-                    <h3>2.) Which year was HTML released?</h3>
+                    <h3>2.) PHP server scripts are surrounded by delimiters, which one of these is it?</h3>
                     
                     <div>
                         <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
-                        <label for="question-2-answers-A">A) 2001</label>
+                        <label for="question-2-answers-A">A) '<?php>....</?>'</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
-                        <label for="question-2-answers-B">B) 1998</label>
+                        <label for="question-2-answers-B">B) <'script'>...</'script'></label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
-                        <label for="question-2-answers-C">C) 1993</label>
+                        <label for="question-2-answers-C">C) <&>...</&></label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
-                        <label for="question-2-answers-D">D) 2003</label>
+                        <label for="question-2-answers-D">D) '<?php ...  ?>'</label>
                     </div>
                 
                 </li>
                 
                 <li>
                 
-                    <h3>3.) What does HTML stand for?</h3>
+                    <h3>3.) All variables in PHP start with which symbol?</h3>
                     
                     <div>
                         <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
-                        <label for="question-3-answers-A">A) Hyper Text Markup Language</label>
+                        <label for="question-3-answers-A">A) !</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
-                        <label for="question-3-answers-B">B) Hyperlinks and Text Markup Language</label>
+                        <label for="question-3-answers-B">B) &</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
-                        <label for="question-3-answers-C">C) Home Tool Markup Language</label>
+                        <label for="question-3-answers-C">C) $</label>
                     </div>
                     
                     <div>
@@ -99,52 +99,52 @@
                 
                 <li>
                 
-                    <h3>4.) A 404 Error is?</h3>
+                    <h3>4.) What is the correct way to add 1 to the $count variable?</h3>
                     
                     <div>
                         <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
-                        <label for="question-4-answers-A">A) is an HTTP Status Code meaning Page Not Found</label>
+                        <label for="question-4-answers-A">A) ++count</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
-                        <label for="question-4-answers-B">B) is a good excuse for a clever design</label>
+                        <label for="question-4-answers-B">B) $count++;</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
-                        <label for="question-4-answers-C">C) should be monitored for in web analytics</label>
+                        <label for="question-4-answers-C">C) count++;</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
-                        <label for="question-4-answers-D">D) All of the above</label>
+                        <label for="question-4-answers-D">D) $count=+1</label>
                     </div>
                 
                 </li>
                 
                 <li>
                 
-                    <h3>5.) Choose the correct HTML element for the largest heading: </h3>
+                    <h3>5.) How do you create an array in PHP? </h3>
                     
                     <div>
                         <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
-                        <label for="question-5-answers-A">A) <'head'></label>
+                        <label for="question-5-answers-A">A) $cars = array("Volvo", "BMW", "Toyota");</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
-                        <label for="question-5-answers-B">B) <'heading'></label>
+                        <label for="question-5-answers-B">B) $cars = array["Volvo", "BMW", "Toyota"];</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
-                        <label for="question-5-answers-C">C) <'h1'></label>
+                        <label for="question-5-answers-C">C) $cars = "Volvo", "BMW", "Toyota";</label>
                     </div>
                     
                     <div>
                         <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
-                        <label for="question-5-answers-D">D) <'h6'></label>
+                        <label for="question-5-answers-D">D) None of the above</label>
                     </div>
                 
                 </li>
