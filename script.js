76f5c5fb8978baed4dc04d675bbfb0aab817b428
xxvisorxx23 <kristjan96@hotmail.com>
2016-11-25 12:50:54 +0000

    laga siðu

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-
-	$('.buttonnav').click(function(){/*þegar það er clickað á button opnar það classið block*/
-
-		$('nav ul').toggleClass('block');/*toggle block classið*/
+/*þegar það er clickað á button opnar það classið block*/
+	$('.buttonnav').click(function(){
+/*toggle block classið*/
+		$('nav ul').toggleClass('block');
 	});
 	
 });
