b8118387e24ea4dc1096d9744292a5bc387b1a6e
xxvisorxx23 <kristjan96@hotmail.com>
2016-11-25 12:49:30 +0000

    addað in javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	$('.buttonnav').click(function(){/*þegar það er clickað á button opnar það classið block*/
+
+		$('nav ul').toggleClass('block');/*toggle block classið*/
+	});
+	
+});
