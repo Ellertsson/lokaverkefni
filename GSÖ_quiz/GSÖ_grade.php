ec7255a3e913cd1c88a890c0bb2c3300b3fbfa8e
ellertsson <benediktel246@nemi.tskoli.is>
2016-11-25 14:24:31 +0000

    rétt svör löguð og if setning fyrir spurningar sem voru ekki svarað bætt við

--- GSÖ_quiz/GSÖ_grade.php
+++ GSÖ_quiz/GSÖ_grade.php
@@ -17,27 +17,36 @@
 		
         <?php
             
-            $answer1 = $_POST['question-1-answers'];
-            $answer2 = $_POST['question-2-answers'];
-            $answer3 = $_POST['question-3-answers'];
-            $answer4 = $_POST['question-4-answers'];
-            $answer5 = $_POST['question-5-answers'];
+            if ( isset($_POST['question-1-answers']) and isset($_POST['question-2-answers']) and  isset($_POST['question-3-answers']) and isset($_POST['question-4-answers']) and isset($_POST['question-5-answers'])  )
+            {
+                  $answer1 = $_POST['question-1-answers'];
+                  $answer2 = $_POST['question-2-answers'];
+                  $answer3 = $_POST['question-3-answers'];
+                  $answer4 = $_POST['question-4-answers'];
+                  $answer5 = $_POST['question-5-answers'];
+                   $Grade = 0;
+                  
+                  if ($answer1 == "A") { $Grade++; }
+                  if ($answer2 == "D") { $Grade++; }
+                  if ($answer3 == "C") { $Grade++; }
+                  if ($answer4 == "B") { $Grade++; }
+                  if ($answer5 == "B") { $Grade++; }
+                  
+                  echo "<div id='results'>$Grade / 5 correct</div>";
+                  if ($Grade == 5) {echo "<div id='results1'>Very Good!</div>";}
+                  if ($Grade == 4) {echo "<div id='results1'>Quite Good!</div>";}
+                  if ($Grade == 3) {echo "<div id='results1'>Just Fine!</div>";}
+                  if ($Grade == 2) {echo "<div id='results1'>Not Good enough!</div>";}
+                  if ($Grade == 1) {echo "<div id='results1'>Not Good at all!</div>";}
+                  if ($Grade == 0) {echo "<div id='results1'>Do you even read?</div>";}
         
-            $Grade = 0;
-            
-            if ($answer1 == "B") { $Grade++; }
-            if ($answer2 == "C") { $Grade++; }
-            if ($answer3 == "A") { $Grade++; }
-            if ($answer4 == "D") { $Grade++; }
-            if ($answer5 == "C") { $Grade++; }
-            
-            echo "<div id='results'>$Grade / 5 correct</div>";
-            if ($Grade == 5) {echo "<div id='results1'>Very Good!</div>";}
-            if ($Grade == 4) {echo "<div id='results1'>Quite Good!</div>";}
-            if ($Grade == 3) {echo "<div id='results1'>Just Fine!</div>";}
-            if ($Grade == 2) {echo "<div id='results1'>Not Good enough!</div>";}
-            if ($Grade == 1) {echo "<div id='results1'>Not Good at all!</div>";}
-            if ($Grade == 0) {echo "<div id='results1'>Do you even read?</div>";}
+            }
+
+          else
+          {
+            echo "A question remains unanswered<br>";
+            echo "<a href='CNA_index.php'>Click here</a> to go back!";
+          }
 
         ?>
 	
