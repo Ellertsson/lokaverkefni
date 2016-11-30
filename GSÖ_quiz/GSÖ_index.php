8dbbdde2d8cd8b5413af93963253616861ff128a
xxvisorxx23 <kristjan96@hotmail.com>
2016-11-30 13:46:43 +0000

    heimasida

---
+++ GSÖ_quiz/GSÖ_index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+	<title>GSÖ Quiz</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<h1>GSÖ Quiz</h1>
+		
+		<form action="VSH_grade.php" method="post" id="quiz">
+		
+            <ol>
+            
+                <li>
+                
+                    <h3>1.) What does PHP stand for?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) Personal Hypertext Processor </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) PHP: Hypertext Preprocessor</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) Private Home Page</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) None of the above</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>2.) PHP server scripts are surrounded by delimiters, which one of these is it?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) <'?php'>....<'/?'></label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) <'script'>...</'script'></label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) <'&'>...<'/&'></label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                        <label for="question-2-answers-D">D) <'?php ...  ?'></label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>3.) All variables in PHP start with which symbol?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) !</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) &</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) $</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                        <label for="question-3-answers-D">D) None of the above</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>4.) What is the correct way to add 1 to the $count variable?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                        <label for="question-4-answers-A">A) ++count</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                        <label for="question-4-answers-B">B) $count++;</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                        <label for="question-4-answers-C">C) count++;</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+                        <label for="question-4-answers-D">D) $count=+1</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>5.) How do you create an array in PHP? </h3>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                        <label for="question-5-answers-A">A) $cars = array("Volvo", "BMW", "Toyota");</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                        <label for="question-5-answers-B">B) $cars = array["Volvo", "BMW", "Toyota"];</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                        <label for="question-5-answers-C">C) $cars = "Volvo", "BMW", "Toyota";</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                        <label for="question-5-answers-D">D) None of the above</label>
+                    </div>
+                
+                </li>
+            
+            </ol>
+            
+            <input type="submit" value="Submit Quiz" />
+		
+		</form>
+	
+	</div>
+    <footer><h3>Benedikt B. Ellertsson & Kristján Ingi - 2016</h3></footer>
+	
+	<script type="text/javascript">
+	document.write(unescape("%3Cscript src='" + Host + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+
+</body>
+
+</html>
--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-/*þegar það er clickað á button opnar það classið block*/
-	$('.buttonnav').click(function(){
-/*toggle block classið*/
-		$('nav ul').toggleClass('block');
-	});
-	
-});
